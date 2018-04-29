370e1bfec0687cbf5f9c7b5e0d0769ef377d7891
bclai7 <bclai27@gmail.com>
2018-04-29 13:13:59 -0400

    Small update to User model
    -Added password attriubute for users
    -Password hashing using bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
 
+  before_save { self.email = email.downcase }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 100},
     format: {with: EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
