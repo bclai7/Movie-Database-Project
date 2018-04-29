b2e5a7da6853b3cf2f29f573fd504500cbee0b0c
bclai7 <bclai27@gmail.com>
2018-04-29 00:26:36 -0400

    Started registration system
    -Added User model
    -Added users controller
    -Added validation for User model attributes (name and email)
    -

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
-  get 'login', to: 'pages#login'
-  get 'register', to: 'pages#register'
+  get  'register',  to: 'users#new'
+
 end
